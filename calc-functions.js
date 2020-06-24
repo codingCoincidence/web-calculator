49fe1f031aef107082cfaae3a3033cd54066ac39
Jakob-Michael Luge <Jakob-Michael.Luge@sivis.com>
2020-06-24 14:20:02 +0200

    assigned mediaqueries

--- calc-functions.js
+++ calc-functions.js
@@ -1,18 +1,26 @@
 function display(e) {
-    if (e.target.id.valueOf() == "clear") {
-        document.getElementById("display").textContent = "";
-    } else {
-        document.getElementById("display").textContent += e.target.textContent;
-    }
+    document.getElementById("display").value += e.target.textContent;
 }
 
-function calculate() {
-    try {
-        var result = eval(document.getElementById("display").textContent);
-    } catch {
-        alert(document.getElementById("display").textContent + " is an invalid expression, please check your input");
-        document.getElementById("display").textContent = "";
-        result = "";
-    }
-    document.getElementById("display").textContent = result;
+function calculate(e) {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
 }
+
+
+
+
+
+
+// function calculate() {
+//     document.getElementById("display").value = eval(document.getElementById("display").value);
+// }
+
+// function calculate() {
+//     try {
+//         var result = eval(document.getElementById("display").value);
+//         document.getElementById("display").value = result;
+//     } catch {
+//         alert(document.getElementById("display").value + " is an invalid expression, please check your input");
+//         document.getElementById("display").textContent = "";
+//     }
+// }
